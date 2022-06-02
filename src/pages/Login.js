f6c48ce84853d981f87b0c6035bccfb3319339be
Cezar <cezarguimaraes@gmail.com>
2022-06-02 08:48:35 -0300

    Style was added. Login page done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #f9f9f9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    position: absolute;
+    font-weight: bolder;
+    font-size: 60px;
+    top: 20%;
+  }
+`;
+
+const LoginBox = styled.div`
+  height: 20vh;
+  width: 50vw;
+  background-color: white;
+  position: absolute;
+  top: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 10px;
+  box-shadow: -1px 1px 3px 3px silver;
+
+  input {
+    width: 70%;
+  }
+
+  button {
+    width: 72%;
+    background-color: blue;
+    font-weight: bolder;
+    color: white;
+    box-shadow: -1px 1px 1px 2px silver;
+  }
+`;
 
 function Login() {
   return (
-    <div data-testid="page-login">
-      <p>Eu sou Login</p>
-    </div>
+    <Container data-testid="page-login">
+      <h1>MyTunes</h1>
+      <LoginBox>
+        <input
+          type="text"
+          placeholder="Nome"
+        />
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </LoginBox>
+    </Container>
   );
 }
 
